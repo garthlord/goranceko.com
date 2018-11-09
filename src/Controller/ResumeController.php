92bad39194d75c523d12a24510505fd4cc4ceebc
Goran Čeko <ceko@intechopen.com>
2018-11-09 04:44:51 +0100

    Added support for creating resume PDFs on the fly

--- src/Controller/ResumeController.php
+++ src/Controller/ResumeController.php
@@ -3,11 +3,16 @@
 namespace App\Controller;
 
 use App\Utils\Resume;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+use Knp\Snappy\Pdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ResumeController extends AbstractController
 {
+    private $_host;
+
     /**
      * @Route("/", name="resume_goran")
      */
@@ -26,5 +31,23 @@ class ResumeController extends AbstractController
         return $this->render('resume/index.html.twig', [
             'resume' => $resume->getResume('jasna'),
         ]);
+    }
+
+    /**
+     * @Route("/resume_download/{person}", name="resume_download")
+     */
+    public function download(Pdf $snappy, $person)
+    {
+        // in dev environment this will not work, you should replace it with real domain
+        // ToDo - find alternative that will work in dev environment also
+        $this->_host = getenv('WEBSITE_URL');
+
+        $route = "resume_{$person}";
+
+        return new PdfResponse(
+            $snappy->getOutput("{$this->_host}{$this->generateUrl($route)}"),
+            "{$route}.pdf"
+        );
+
     }
 }
